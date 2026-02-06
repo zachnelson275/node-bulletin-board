eb45a13351d5c4ae090b5100084ed6e0a29598a9
Zach Nelson <zachnelson275@gmail.com>
2026-02-06 14:48:29 -0700

    jest config file

---
+++ bulletin-board-app/jest.config.js
@@ -0,0 +1,12 @@
+const config = {
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: -10,
+        },
+    },
+};
+
+module.exports = config;
